7c0bdc375ad4b3b640606848931e3869893705c8
Daniel from Phcity <103477809+CoderOfPHCity@users.noreply.github.com>
2024-02-16 21:06:44 +0100

    Update Lock.sol

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,32 +1,31 @@
-// // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
-
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 contract DepositWithdraw {
-
-
     mapping(address => mapping(address => uint256)) tokenBalances;
 
     function deposit(address token, uint256 amount) external payable {
-        require( msg.value > 0, "not enough deposit");
-
+        require(msg.value > 0 || amount > 0, "Invalid deposit");
+        
+        if (token == address(0)) {
             tokenBalances[msg.sender][address(0)] += msg.value;
-            require(amount > 0, "Cannot deposit zero tokens");
-           
+        } else {
             tokenBalances[msg.sender][token] += amount;
         }
+    }
 
-    function withdraw(address token,address _to, uint256 amount) external {
-        require( amount > 0, "bnot enough");
+    function withdraw(address token, uint256 amount) external {
+        require(amount > 0, "Invalid withdrawal");
         
-            tokenBalances[msg.sender][_to] -= amount;
+        if (token == address(0)) {
+            require(tokenBalances[msg.sender][address(0)] >= amount, "Insufficient Ether balance");
+            tokenBalances[msg.sender][address(0)] -= amount;
             payable(msg.sender).transfer(amount);
-            require(tokenBalances[msg.sender][token] >= amount, "Insufficient e");
+        } else {
+            require(tokenBalances[msg.sender][token] >= amount, "Insufficient token balance");
             tokenBalances[msg.sender][token] -= amount;
         }
+    }
 
     function getBalance(address token) external view returns (uint256) {
         return token == address(0) ? tokenBalances[msg.sender][address(0)] : tokenBalances[msg.sender][token];
